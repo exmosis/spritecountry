e8c94bfbd09e683441a5e9a9e45ff8c4b9fc288b
Graham <janus-git@exmosis.net>
2021-03-06 21:06:30 +0000

    Tidy code up

--- httpdocs/.inc/Exmosis/autoload.php
+++ httpdocs/.inc/Exmosis/autoload.php
@@ -6,7 +6,6 @@ spl_autoload_register(function($class) {
 	$class = preg_replace('/^' . __NAMESPACE__ . '\\\/', '', $class);
 	$file = dirname(__FILE__) . DIRECTORY_SEPARATOR . $class . '.php';
 	$file = preg_replace('/\\\\/', DIRECTORY_SEPARATOR, $file);
-	// echo "Autoload: $file\n";
 	require($file);
 });
 
