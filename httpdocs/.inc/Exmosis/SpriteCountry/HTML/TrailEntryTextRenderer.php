f5385d6e0c8988a7f4ac60c185c7e5c54d01e578
Graham <janus-git@exmosis.net>
2021-03-12 14:20:54 +0000

    New Text renderer

---
+++ httpdocs/.inc/Exmosis/SpriteCountry/HTML/TrailEntryTextRenderer.php
@@ -0,0 +1,35 @@
+<?php 
+
+namespace Exmosis\SpriteCountry\HTML;
+
+use Exmosis\SpriteCountry\Domain\TrailEntry;
+use Exmosis\SpriteCountry\Domain\TrailEntryText;
+
+use Exception;
+
+class TrailEntryTextRenderer extends TrailEntryRenderer {
+    
+    public function __construct(TrailEntry $trail_entry) {
+        
+        if (! $trail_entry instanceof TrailEntryText) {
+            throw Exception('TrailEntryTextRenderer needs a TrailEntryText');
+        }
+        
+        $this->trail_entry = $trail_entry;
+    }
+    
+    public function getHtml() {
+        return $this->getTextHtml();
+    }
+    
+    public function getTextHtml() {
+        $text = $this->trail_entry->getText();
+        $text = $this->wrapInLink($text);
+
+        $html = '<div id="text_container">';
+        $html .= '<div id="trail_text">' . $text . '</div>';
+        $html .= '</div>';
+        return $html;
+    }   
+    
+}
