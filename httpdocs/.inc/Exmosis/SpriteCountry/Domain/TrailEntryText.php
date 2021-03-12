b9e4a450f2cfb4535bc0d1f642be8571be612689
Graham <janus-git@exmosis.net>
2021-03-12 14:21:11 +0000

    Remove rendering from Text entries

--- httpdocs/.inc/Exmosis/SpriteCountry/Domain/TrailEntryText.php
+++ httpdocs/.inc/Exmosis/SpriteCountry/Domain/TrailEntryText.php
@@ -13,22 +13,8 @@ class TrailEntryText extends TrailEntry {
 		$this->text = $entry_data[TrailEntry::KEY__TEXT];
 	}
 	
-	/**
-	 * TODO: Move this into a separate renderer.
-	 * 
-	 * {@inheritDoc}
-	 * @see \Exmosis\SpriteCountry\Domain\TrailEntry::getHtml()
-	 */
-	public function getHtml() {
-		$html = '<div id="text_container">';
-		$html .= '<div id="trail_text">' . $this->text . '</div>';
-		if (! is_null($this->getNext())) {
-			$html .= '<div>' . $this->getNextLinkOpen() . '&gt;' . $this->getNextLinkClose() . '</div>';
-		}
-		$html .= '</div>';
-		return $html;
-	} 
-	
-
+	public function getText() {
+	    return $this->text;
+	}
 
 }
