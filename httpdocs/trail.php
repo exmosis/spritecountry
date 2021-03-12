14dba657bbd339cb400e1dc59f914a2026833e3d
Graham <janus-git@exmosis.net>
2021-03-12 11:12:21 +0000

    Get trail page working after refactors

--- httpdocs/trail.php
+++ httpdocs/trail.php
@@ -1,29 +1,29 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // composer load.
 require_once '.inc' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php'; 	
-// exmosis load - TODO: Autoload
-require_once '.inc' . DIRECTORY_SEPARATOR . 'Exmosis' . DIRECTORY_SEPARATOR . 'SpriteCountry' . DIRECTORY_SEPARATOR . 'Header.php';
-require_once '.inc' . DIRECTORY_SEPARATOR . 'Exmosis' . DIRECTORY_SEPARATOR . 'SpriteCountry' . DIRECTORY_SEPARATOR . 'Footer.php';
 
-require_once '.inc' . DIRECTORY_SEPARATOR . 'Exmosis' . DIRECTORY_SEPARATOR . 'SpriteCountry' . DIRECTORY_SEPARATOR . 'Trail.php';
-require_once '.inc' . DIRECTORY_SEPARATOR . 'Exmosis' . DIRECTORY_SEPARATOR . 'SpriteCountry' . DIRECTORY_SEPARATOR . 'TrailEntry.php';
-require_once '.inc' . DIRECTORY_SEPARATOR . 'Exmosis' . DIRECTORY_SEPARATOR . 'SpriteCountry' . DIRECTORY_SEPARATOR . 'TrailEntryText.php';
-require_once '.inc' . DIRECTORY_SEPARATOR . 'Exmosis' . DIRECTORY_SEPARATOR . 'SpriteCountry' . DIRECTORY_SEPARATOR . 'TrailEntryImage.php';
+// exmosis load
+require_once '.inc' . DIRECTORY_SEPARATOR . 'Exmosis' . DIRECTORY_SEPARATOR . 'autoload.php';
 
+use Exmosis\SpriteCountry\HTTP\SpriteCountryRequest;
+use Exmosis\SpriteCountry\Data\SpriteCountryTrailData;
+use Exmosis\SpriteCountry\Data\SpriteCountryData;
 
-require_once '.inc' . DIRECTORY_SEPARATOR . 'Exmosis' . DIRECTORY_SEPARATOR . 'SpriteCountry' . DIRECTORY_SEPARATOR . 'TrailRequest.php';
-require_once '.inc' . DIRECTORY_SEPARATOR . 'Exmosis' . DIRECTORY_SEPARATOR . 'SpriteCountry' . DIRECTORY_SEPARATOR . 'SpriteCountryRequest.php'; 	
-require_once '.inc' . DIRECTORY_SEPARATOR . 'Exmosis' . DIRECTORY_SEPARATOR . 'SpriteCountry' . DIRECTORY_SEPARATOR . 'SpriteCountryData.php'; 	
+$sctd = new SpriteCountryTrailData('data/trails.csv');
+$sctd->load();
 
-// TODO: Pass this round
-$scd = new Exmosis\SpriteCountry\SpriteCountryData('data/trail_entries.csv');
+$scd = new SpriteCountryData('data/trail_entries.csv');
 $scd->load();
 
 // oh wait, the Request object handles types of URL, duh.
 // TODO: turn trail.php into index.php
 if (array_key_exists('path', $_GET)) {
-	$req = new Exmosis\SpriteCountry\SpriteCountryRequest($_GET['path']);
+	$req = new SpriteCountryRequest($_GET['path'], $sctd, $scd);
 	$req->process();
 }
 
