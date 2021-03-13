f49f6b9062bde664404104044c8c657c1339668d
Graham <janus-git@exmosis.net>
2021-03-13 17:09:10 +0000

    Really basic config file

---
+++ httpdocs/.inc/Exmosis/SpriteCountry/HTML/HtmlConfig.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Exmosis\SpriteCountry\HTML;
+
+class HtmlConfig {
+    
+    // All web paths should start and end with '/'
+    const WEBPATH__SITE_IMAGES = '/images/';
+    const WEBPATH__TRAIL_IMAGES = '/data/img/';
+    
+}
